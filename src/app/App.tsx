d4db3dd44d298d3eac7280571066ad6b4f24df7f
NeighborsRestock <mikhail@neighborsrestock.com>
2025-02-13 12:30:43 -0500

    Pushing final site

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,22 +3,50 @@ import { BrandsWeCarry, Landing, Contact, NotFound } from "@/pages";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer/Footer";
 import { APP_ROUTES } from "@/config/routes";
+import FloatingLabelInput from "@/components/FloatingInput/FloatingInput";
+import { SetStateAction, ChangeEvent, useState, useEffect } from "react";
 
 function App() {
   const { pathname } = useLocation();
   const willHideFooter = pathname === APP_ROUTES.NOT_FOUND;
+  const [passcode, setPasscode] = useState("");
+  const [showSite, setShowSite] = useState(false);
+
+  useEffect(() => {
+    if (passcode === import.meta.env.VITE_PASSCODE) setShowSite(true);
+  });
 
   return (
     <>
-      <Header />
-      <Routes>
-        <Route index path={`${APP_ROUTES.LANDING}`} element={<Landing />} />
-        <Route path={`${APP_ROUTES.BRANDS}`} element={<BrandsWeCarry />} />
-        <Route path={`${APP_ROUTES.CONTACT}`} element={<Contact />} />
-        <Route path="*" element={<Navigate to={`${APP_ROUTES.NOT_FOUND}`} />} />
-        <Route path={`${APP_ROUTES.NOT_FOUND}`} element={<NotFound />} />
-      </Routes>
-      {!willHideFooter && <Footer />}
+      {!showSite && (
+        <div className="w-screen h-screen flex justify-center items-center bg-[0e1b23]">
+          <FloatingLabelInput
+            label={""}
+            value={passcode}
+            onFieldChange={function (
+              e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+            ): void {
+              setPasscode(e.target.value);
+            }}
+          />
+        </div>
+      )}
+      {showSite && (
+        <>
+          <Header />
+          <Routes>
+            <Route index path={`${APP_ROUTES.LANDING}`} element={<Landing />} />
+            <Route path={`${APP_ROUTES.BRANDS}`} element={<BrandsWeCarry />} />
+            <Route path={`${APP_ROUTES.CONTACT}`} element={<Contact />} />
+            <Route
+              path="*"
+              element={<Navigate to={`${APP_ROUTES.NOT_FOUND}`} />}
+            />
+            <Route path={`${APP_ROUTES.NOT_FOUND}`} element={<NotFound />} />
+          </Routes>
+          {!willHideFooter && <Footer />}
+        </>
+      )}
     </>
   );
 }
--- src/components/FloatingInput/FloatingInput.types.ts
+++ src/components/FloatingInput/FloatingInput.types.ts
@@ -2,13 +2,13 @@ import { ContactFormInputs } from "@/hooks/types";
 import { ChangeEvent } from "react";
 
 export type FloatingLabelInputProps = {
-  id: ContactFormInputs;
+  id?: ContactFormInputs;
   type?: string;
   label: string;
   value: string;
   pattern?: string;
   className?: string;
-  isSubmitActive: boolean;
-  setIsSubmitActive: React.Dispatch<React.SetStateAction<boolean>>;
+  isSubmitActive?: boolean;
+  setIsSubmitActive?: React.Dispatch<React.SetStateAction<boolean>>;
   onFieldChange: (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, callback?: () => void) => void;
 };
